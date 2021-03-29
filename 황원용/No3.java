bf525b029ce6edbf42db2c2c1d3d8d0af822738e
anndy2231 <5jose7@naver.com>
2021-03-29 18:18:06 +0900

    황원용

---
+++ 황원용/No3.java
@@ -0,0 +1,10 @@
+
+public class No3 {
+
+	public static void main(String[] args) {
+		System.out.println("구구단 : 6단");
+			int i = 6;
+			for (int j = 1; j <= 9; j++)
+				System.out.println(i + "X" + j + "=" + i * j);
+		}
+	}

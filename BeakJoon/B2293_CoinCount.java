f37362b5590124387a3f8d183982f7b9bcd8e67a
anndy2231 <5jose7@naver.com>
2021-05-31 22:58:43 +0900

    edit

--- BeakJoon/B2293_CoinCount.java
+++ BeakJoon/B2293_CoinCount.java
@@ -1,29 +1,38 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class B2293_CoinCount {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int n = sc.nextInt(); // 동전 종류
-		int k = sc.nextInt(); // 동전 가치의 합
+		String arr [] = br.readLine().split(" ");
+		int n = Integer.parseInt(arr[0]); // 동전 종류 
+		int k = Integer.parseInt(arr[1]); // 동전 가치의 합
 		
 		int coin[] = new int [101]; // 동전 종류는 1~100
-		int dp[] = new int[10001]; // 동전 가치의 합은 1~10000
+		int dp[] = new int [10001]; // 동전 가치의 합은 1~10000
 		
-		for (int i = 1; i <= n; i++) {
-			coin[i] = sc.nextInt();
+		/* 			0	1	2	3	4	5	6	7	8	9	10
+		 * 1		1	1 	1	1	1	1	1	1	1	1	1	
+		 * 1,2		1	1	2	2	3	3	4	4	5	5	6
+		 * 1,2,5	1	1	2	2	3	4	5	6	7	8	*10	
+		 * 	
+		 */
+		
+		for (int i = 0; i < n; i++) {
+			coin[i] = Integer.parseInt(br.readLine());
 		}
-		System.out.println(Arrays.toString(coin));
 		
-		dp[0] = 1;
-		for (int i = 1; i <= n; i++) {
-			for (int j = coin[i]; j <=k; j++) {
-				dp[j] += dp[j-coin[i]];
+		dp[0]=1;
+
+		for (int i = 0; i < n; i++) {
+			for (int j = coin[i]; j <= k; j++) {
+				dp[j] = dp[j]+dp[j-coin[i]];
 			}
 		}
-		System.out.println(Arrays.toString(dp));
 		System.out.println(dp[k]);
-		sc.close();
 	}
 }
